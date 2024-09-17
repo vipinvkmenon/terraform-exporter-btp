3f887f738dab22f4179effe8c6b5dea66f4e06ec
Anugrah Gupta <anugrah.gupta@sap.com>
2024-09-17 15:40:30 +0530

    skip failed and in-progress subscriptions (#47)

--- cmd/exportSubaccountSubscriptionsHelper.go
+++ cmd/exportSubaccountSubscriptionsHelper.go
@@ -43,6 +43,8 @@ func getSubscriptionsImportBlock(data map[string]interface{}, subaccountId strin
 	var importBlock string
 	subscriptions := data["values"].([]interface{})
 
+	var failedSubscriptions []string
+	var inProgressSubscription []string
 	if len(filterValues) != 0 {
 		var subaccountAllSubscriptions []string
 
@@ -50,7 +52,13 @@ func getSubscriptionsImportBlock(data map[string]interface{}, subaccountId strin
 			subscription := value.(map[string]interface{})
 			subaccountAllSubscriptions = append(subaccountAllSubscriptions, fmt.Sprintf("%v", subscription["app_name"])+"_"+fmt.Sprintf("%v", subscription["plan_name"]))
 			if slices.Contains(filterValues, fmt.Sprintf("%v", subscription["app_name"])+"_"+fmt.Sprintf("%v", subscription["plan_name"])) {
-				importBlock += templateSubscriptionImport(subscription, subaccountId, resourceDoc)
+				if fmt.Sprintf("%v", subscription["state"]) == "SUBSCRIBED" {
+					importBlock += templateSubscriptionImport(subscription, subaccountId, resourceDoc)
+				} else if fmt.Sprintf("%v", subscription["state"]) == "SUBSCRIBE_FAILED" {
+					failedSubscriptions = append(failedSubscriptions, fmt.Sprintf("%v", subscription["app_name"])+"_"+fmt.Sprintf("%v", subscription["plan_name"]))
+				} else if fmt.Sprintf("%v", subscription["state"]) == "IN_PROCESS" {
+					inProgressSubscription = append(inProgressSubscription, fmt.Sprintf("%v", subscription["app_name"])+"_"+fmt.Sprintf("%v", subscription["plan_name"]))
+				}
 			}
 		}
 
@@ -63,10 +71,25 @@ func getSubscriptionsImportBlock(data map[string]interface{}, subaccountId strin
 	} else {
 		for _, value := range subscriptions {
 			subscription := value.(map[string]interface{})
-			if fmt.Sprintf("%v", subscription["state"]) != "NOT_SUBSCRIBED" {
+			if fmt.Sprintf("%v", subscription["state"]) == "SUBSCRIBED" {
 				importBlock += templateSubscriptionImport(subscription, subaccountId, resourceDoc)
+			} else if fmt.Sprintf("%v", subscription["state"]) == "SUBSCRIBE_FAILED" {
+				failedSubscriptions = append(failedSubscriptions, fmt.Sprintf("%v", subscription["app_name"])+"_"+fmt.Sprintf("%v", subscription["plan_name"]))
+
+			} else if fmt.Sprintf("%v", subscription["state"]) == "IN_PROCESS" {
+				inProgressSubscription = append(inProgressSubscription, fmt.Sprintf("%v", subscription["app_name"])+"_"+fmt.Sprintf("%v", subscription["plan_name"]))
+
 			}
 		}
+	}
+
+	if len(failedSubscriptions) != 0 {
+		failedSubscriptionsStr := strings.Join(failedSubscriptions, ", ")
+		log.Println("Skipping failed subscriptions: " + failedSubscriptionsStr)
+	}
+	if len(inProgressSubscription) != 0 {
+		inProgressSubscriptionStr := strings.Join(inProgressSubscription, ", ")
+		log.Println("Skipping in progress subscriptions: " + inProgressSubscriptionStr)
 	}
 
 	return importBlock, nil
