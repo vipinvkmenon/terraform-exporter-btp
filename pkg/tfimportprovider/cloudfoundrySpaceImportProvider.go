527701543d56d913117b761f6a113f4c5dd5a70a
Vipin Menon <vipinvkmenon@users.noreply.github.com>
2025-05-07 11:50:00 +0530

    fix: Space resource name creation (#379)
    - Follow the approach with rest of the resources

--- pkg/tfimportprovider/cloudfoundrySpaceImportProvider.go
+++ pkg/tfimportprovider/cloudfoundrySpaceImportProvider.go
@@ -73,7 +73,7 @@ func createSpaceImportBlock(data map[string]interface{}, orgId string, filterVal
 }
 
 func templateSpaceImport(x int, space map[string]interface{}, resourceDoc tfutils.EntityDocs) string {
-	template := strings.ReplaceAll(resourceDoc.Import, "<resource_name>", "space_"+fmt.Sprintf("%v", space["name"]))
+	template := strings.ReplaceAll(resourceDoc.Import, "<resource_name>", "space_"+fmt.Sprintf("%v", x))
 	template = strings.ReplaceAll(template, "<space_guid>", fmt.Sprintf("%v", space["id"]))
 	return template + "\n"
 }
